324deb3baf4d7baae85491be62dd788a7ce42564
Juan020206 <jtobares@alumno.huergo.edu.ar>
2025-06-18 19:15:28 -0300

    agrego el ejercicio wifi terminado

--- ejericiciowifi/ejericiciowifi.ino
+++ ejericiciowifi/ejericiciowifi.ino
@@ -8,8 +8,10 @@ PubSubClient client(espClient);
 // Callback (una función con una firma particular) que responderá ante un nuevo mensaje recibido
 void callback(char* topic, byte* payload, unsigned int length) {
   // Iteramos el payload
+  char letra;
   for (int i = 0; i < length; i++) {
-    
+  letra=(char)payload[i];
+  Serial.print(letra);
   }
   Serial.println();
 }
@@ -18,12 +20,19 @@ void setup() {
   Serial.begin(115200);
 
   // Conectarse a una red por nombre + contraseña
-  WiFi.begin("---", "----");
+  WiFi.begin("Wifi", "Contraseña");
 
-  if (WiFi.status() != WL_CONNECTED){
-    // aun no esta conectado
+
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.println("No se pudo conectar");
+    Serial.println("Reintentando...");
+    delay(4000);
   }
-  // ok
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("Conectado");
+
+  }
+
 
   // Realizamos la conexión con el servidor
   client.setServer("broker.hivemq.com", 1883);
@@ -31,8 +40,15 @@ void setup() {
   client.connect("ESP32_SENSOR_01"); // devuelve un booleano indicando si se conectó correctamente
 
   client.connected(); // devuelve un booleano que nos permite chequear si la conexión fue exitosa
-
-  client.subscribe("casa/sala/temperatura"); // seguir patrón del tópico a escuchar
+  while(!client.connected()){
+    Serial.println("Error de conexion de broker");
+    Serial.println("Reintentando");
+    delay(3000);
+  }
+  if(client.connected()){
+    Serial.println("conectado al broker");
+  }
+  client.subscribe("huergo/sistemas-embebidos/Tobares"); // seguir patrón del tópico a escuchar
 
   // Configuramos el cliente con el callback correcto
   client.setCallback(callback);
@@ -41,9 +57,9 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String msg = Serial.readStringUntil('\n');
+    String msg_user= "Tobares:"+ msg;
+    client.publish("huergo/sistemas-embebidos/Tobares", msg.c_str()); // convertir el mensaje mediante .c_str()
+    client.loop();
   }
-
-  client.publish("casa/sala/temperatura", "13 °C"); // convertir el mensaje mediante .c_str()
-
-  client.loop();
 }
+
